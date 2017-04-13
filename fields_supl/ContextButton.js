21fac0d09dd0f2314af1ef3eb9a1c34b3dc2a2f9
Stephen Francis <stephen.francis@rullion.co.uk>
2017-04-13 17:56:18 +0100

    make ContextButton work as an in-page reload

--- fields_supl/ContextButton.js
+++ fields_supl/ContextButton.js
@@ -9,33 +9,64 @@ var Data = require("lazuli-data/index.js");
 module.exports = Data.Text.clone({
     id: "ContextButton",
     css_type: "url",
+    editable: false,            // no reason to be editable...
     label: "",                  // Mainly just the column heading
+    tb_size: "xs",
+    // default_val: "#",
+    btn_css_class: null,
     url_pattern: "{val}",       // set url_pattern to "mailto:{val}" for email address fields
     text_pattern: "",           // show icon only, not the text of the URL
     sql_function: "NULL",       // not stored in db!
 });
 
 
-module.exports.override("renderUneditable", function (elem, render_opts) {
-    var style;
+module.exports.override("isVisible", function (field_group, hide_blank_uneditable) {
+    return this.visible && (this.accessible !== false)
+            && (!field_group || field_group === this.field_group)
+            && (!this.hide_if_no_link || this.getURL());
+});
 
+
+module.exports.override("renderControl", function (div_elmt, render_opts, form_type) {
+    var style;
+    var anchor_elmt;
     if (!this.validated) {
         this.validate();
     }
     if (this.getText() !== this.val) {
-        elem.attribute("val", this.val);
+        div_elmt.attr("val", this.val);
     }
     style = this.getUneditableCSSStyle();
     if (style) {
-        elem.attribute("style", style);
+        div_elmt.attr("style", style);
     }
     if (!this.url) {
         this.getURL();
     }
-    if (this.url && render_opts.show_links !== false) {
-        elem.makeElement("a")
-            .attr("class", "btn btn-mini")       // removed css_open_in_modal
-            .attr("href", this.url)
-            .text(this.btn_label, true);
+    if (render_opts.show_links === false) {
+        return;
     }
+    if (this.css_cmd || this.url) {
+        anchor_elmt = div_elmt.makeElement("a")
+            .attr("class", this.getButtonCSSClass());
+        if (this.url) {
+            anchor_elmt.attr("href", this.url);
+        }
+        if (this.css_cmd) {
+            anchor_elmt.attr("id", this.getControl());
+        }
+        anchor_elmt.text(this.btn_label, true);
+    }
+});
+
+
+module.exports.define("getButtonCSSClass", function () {
+    var css_class = "btn btn-" + this.tb_size;
+    if (this.btn_css_class) {
+        css_class += " " + this.btn_css_class;
+    }
+    if (this.css_cmd) {
+        css_class += " css_cmd";
+    }
+    return css_class;
 });
